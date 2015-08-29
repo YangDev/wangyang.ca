f6b435323541d36638486012a2dabb7ea1208c07
Yang2008 <ywang248@gmail.com>
2015-08-29 06:04:29 -0400

    init

---
+++ public/js/ml/recognition.js
@@ -0,0 +1,496 @@
+angular.module("myApp", []).controller("pageController", function ($scope, $http) {
+	$scope.buttonStyle="";
+	$scope.result = "";
+	$scope.inputLabel = "";
+	$scope.sample = "";
+	$scope.mode = 0;
+	//$scope.active = 
+	$scope.setModeTrain = function () {
+		$scope.tab1 = 2;
+	};
+
+	$scope.setModeLearn = function () {
+		$scope.tab1 = 1;
+	};
+
+	$scope.setModeAuto = function () {
+		$scope.tab2 = 2;
+	};
+
+	$scope.setModeManual = function () {
+		$scope.tab2 = 1;
+	};
+
+	$scope.learn = function () {
+		trainAll(samples, labels, WEIGHTS);
+	};
+
+	$scope.recognize = function () {
+		perdict();
+		clearContext();
+	};
+
+	$scope.add = function () {
+		if (label[0].value !== "") {
+			var temp = label[0].value;
+			imgData = ctx_img.getImageData(0, 0, image.width, image.height);
+			//updateImgArray(imgData, imgArray);
+			imgLib.push(imgData);
+			addSample(samples, createImgArray(imgData), temp);
+			//samples.push({data: createImgArray(imgData), label: temp});
+			addLabel(temp);
+			WEIGHTS.push(createWeight(400));
+			sampleLabel.push(temp);
+			label[0].value = "";
+			ctx_img.clearRect(0, 0, 20, 20);
+			ctx.clearRect(0, 0, cvs.width, cvs.height);
+		}
+	};
+
+	$scope.addOneSample = function () {
+		addSample(samples, createImgArray(imgData), $scope.result);
+		$scope.mode = 0;
+		$scope.learn();
+		$scope.sample += $scope.result;
+		console.log(samples);
+		clearContext();
+	};
+
+	$scope.addNewSample = function () {
+		if ($scope.inputLabel !== "") {
+			addSample(samples, createImgArray(imgData), $scope.inputLabel);
+			
+			WEIGHTS.push(createWeight(400));
+			sampleLabel.push($scope.inputLabel);
+			if (labels.indexOf($scope.inputLabel) === -1) {
+				addLabel($scope.inputLabel);
+				$http.post('/weight', {label: $scope.inputLabel, weight: WEIGHTS[WEIGHTS.length - 1]});
+			}
+			$scope.learn();
+			$scope.sample += $scope.inputLabel;
+			$scope.inputLabel = "";
+			$scope.mode = 0;
+			console.log(samples);
+			clearContext();
+		} else{
+			console.log("input label is empty");
+		}
+	};
+
+
+	$scope.getLabel = function () {
+		$scope.mode = 2;
+	}
+
+
+	console.log(screen.width);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	var down = 0;
+	var cvs = document.getElementById("input");
+	var image = document.getElementById("image");
+	//var b1 = document.getElementById("b1");
+	
+	//var b2 = document.getElementById("b2");
+	//var b3 = document.getElementById("b3");
+	//var b4 = document.getElementById("b4");
+	var label = document.getElementsByTagName("input");
+
+	
+	image.width = 20;
+	image.height = 20;
+	if (screen.width < 600) {
+		cvs.width = screen.width;
+		cvs.height = screen.width;
+	} else {
+		cvs.width = 600;
+		cvs.height = 600;
+	}
+	var ctx = cvs.getContext('2d');
+	var ctx_img = image.getContext('2d');
+	var offset_l = ctx_img.canvas.offsetLeft;
+	var offset_t = ctx_img.canvas.offsetTop;
+	var ratio = cvs.width / image.width
+	var samples = [];
+	var sampleLabel = [];
+	var labels = [];
+	var WEIGHTS = [];
+	var output;
+	var imgData;
+	var imgLib = [];
+	var idleTime;
+	//console.log(ctx_img);
+	//console.log(offset_l);
+	var imgArray = [];
+	var sigmoid = function (x) {
+		return 1 / (1 + Math.exp(-x));
+	}
+
+	var addSample = function (array, sample, label) {
+		array.push({data: sample, label:label});
+		$http.post('/sample', {data: sample, label:label});
+	};
+
+	var sigmoidGradient = function (a) {
+		return a * (1 - a);
+	};
+	var updateImgArray = function (imgdata, array) {
+		var i, j;
+		j = 0;
+		for (i = 3; i < imgdata.data.length; i += 4) {
+			array[j] = imgdata.data[i] / 255 / 400;
+			j++;
+		};
+	};
+
+	var createImgArray = function (imgdata) {
+		var array = [];
+		var i, j;
+		j = 0;
+		for (i = 3; i < imgdata.data.length; i += 4) {
+			array[j] = imgdata.data[i] / 102000;
+			j++;
+		};
+		return array;
+	};
+
+	var restoreImgArray = function (imgArray) {
+		var n = imgArray.length * 4;
+		var array = [];
+		var i, j, k;
+		k = 0;
+		j = 0;
+		for (i = 0; i < n; i++) {
+			if (j === 3) {
+				array[i] = imgArray[k] * 102000;
+				j = 0;
+			} else {
+				array[i] = 0;
+			}
+			j++;
+			k++;
+		}
+		return array;
+	};
+
+	var createWeight = function (numberWeights) {
+		var i;
+		var weight = [];
+		for (i = 0; i < numberWeights; i++) {
+			weight[i] = 0.45 + Math.random() * 0.1;
+		};
+		return weight;
+	}
+	
+	var feedForward = function (input, numberOutput) {
+		var outputActivation = []
+		var sum;
+		var i, j;
+		//console.log(input, WEIGHTS);
+		for (i = 0; i < numberOutput; i++){
+			sum = 0.0;
+			for (j = 0; j < input.length; j++) {
+				//console.log(weights[i][j]);
+				sum += input[j] * WEIGHTS[i][j];
+				//console.log(sum);
+			};
+			//console.log("sum is " + sum);
+			outputActivation[i] = sigmoid(sum);
+		}
+		return outputActivation;
+	}
+
+	var getDelta = function (output, trueLabel, labels) {
+		var delta = [];
+		for (var i = 0; i < labels.length; i++) {
+			if (labels[i] == trueLabel) {
+				delta[i] = 1.0 - output[i];
+			} else {
+				delta[i] = 0.0 - output[i];
+			}
+		};
+		return delta;
+	}
+
+	var trainAll = function (inputs, labels) {
+		var error1 = 0;
+		var error2 = 0;
+		var error3 = 0;
+		var i, j;
+		for (i = 0; i < 2000; i++) {
+			for (j = 0; j < inputs.length; j++) {
+				train(inputs[j], labels, inputs[j].label);
+			};
+		}
+		error1 = train(inputs[0], labels, inputs[0].label);
+		console.log(error1);
+		//console.log(WEIGHTS);
+	}
+
+	//l: true label
+	var train = function (input, labels, l) {
+		var output = feedForward(input.data, labels.length);
+		var delta = [];
+		var i, j;
+		//console.log(labels);
+		delta = getDelta(output, l, labels);
+		for (i = 0; i < labels.length; i++) {
+			for (j = 0; j < WEIGHTS[i].length; j++) {
+				//console.log(delta[i] , input[j] ,5， sigmoidGradient(output[i]));
+				WEIGHTS[i][j] += 100 * delta[i] * input.data[j] * sigmoidGradient(output[i]);
+			}
+		}
+		//console.log(WEIGHTS)
+		return delta;
+	}
+	var getX = function (context, e) {
+		return e.pageX - context.canvas.offsetLeft;
+	};
+
+	var getY = function (context, e) {
+		return e.pageY - context.canvas.offsetTop - window.pageYOffset;
+	};
+	var beginDraw = function (e, context, ratio) {
+		context.beginPath();
+		//var x = e.pageX - 
+		context.moveTo(getX(context, e) / ratio, getY(context, e) / ratio);
+	};
+
+	var endDraw = function (e, context, ratio) {
+		context.lineTo(getX(context, e) / ratio, getY(context, e) / ratio);
+		context.stroke();
+	};
+
+	var createImg = function (imgData, label) {
+		var img = [];
+		updateImgArray(imgData, img);
+		return {
+			data:img,
+			label:label
+		}
+	};
+
+	var clearContext = function () {
+		ctx_img.clearRect(0, 0, 20, 20);
+		ctx.clearRect(0, 0, cvs.width, cvs.height);
+	};
+
+	var addLabel = function (l) {
+		if (labels.indexOf(l) === -1) {
+			labels.push(l);
+		}
+	};
+
+	var countSecond = function () {
+		console.log("good");
+	}
+
+	var promptLearn = function () {
+
+	}
+	var process = function () {
+		var idx = -1;
+		imgData = ctx_img.getImageData(0, 0, image.width, image.height);
+		updateImgArray(imgData, imgArray);
+		var p = feedForward(imgArray, labels.length);
+		if (p[0] === undefined) {
+			//promptLearn();
+			console.log("empty");
+			$scope.mode = 2;
+		} else {
+			$scope.mode = 1;
+			idx = maxIndex(p);
+			$scope.result = labels[idx];
+			console.log($scope.result);
+			console.log(labels[idx]);
+			console.log(p);
+		}
+		$scope.$apply();
+	}
+
+
+	var maxIndex = function (array) {
+		var idx = -1;
+		var max = 0;
+		for (var i = 0; i < array.length; i++) {
+			if (max < array[i]) {
+				max = array[i];
+				idx = i;
+			}
+		};
+		return idx;
+	}
+
+
+	//console.log(maxIndex([1,2,3,5,4]));
+	
+//***************** event listeners **************************
+	/*//button:train 
+	b1.addEventListener("click", function () {
+		trainAll(samples, labels, WEIGHTS);
+	});
+
+
+	//button:add sample 
+	b2.addEventListener("click", function () {
+		if (label[0].value !== "") {
+			var temp = label[0].value;
+			imgData = ctx_img.getImageData(0, 0, image.width, image.height);
+			//updateImgArray(imgData, imgArray);
+			imgLib.push(imgData);
+			samples.push(createImgArray(imgData));
+			addLabel(temp);
+			WEIGHTS.push(createWeight(400));
+			sampleLabel.push(temp);
+			label[0].value = "";
+			ctx_img.clearRect(0, 0, 20, 20);
+			ctx.clearRect(0, 0, 600, 600);
+		}
+
+		//console.log(samples);
+		//console.log(labels);
+		//console.log(weights);
+	});
+
+	//perdict
+	b3.addEventListener("click", function () {
+			imgData = ctx_img.getImageData(0, 0, image.width, image.height);
+			updateImgArray(imgData, imgArray);
+			var p = feedForward(imgArray, labels.length);
+			console.log(p);
+			ctx_img.clearRect(0, 0, 20, 20);
+			ctx.clearRect(0, 0, 600, 600);
+
+		//console.log(samples);
+		//console.log(labels);
+		//console.log(weights);
+	});*/
+
+	cvs.addEventListener('mousedown', function(e){
+		down = 1;
+		clearTimeout(idleTime);
+		beginDraw(e, ctx, 1);
+		beginDraw(e, ctx_img, ratio);
+	});
+
+	
+	cvs.addEventListener('mouseup', function(e){
+		down = 0;
+		idleTime = setTimeout(process, 1000)
+	});
+
+	cvs.addEventListener('mousemove', function(e){
+		console.log(cvs);
+		if (down === 1) {
+			endDraw(e, ctx, 1);
+			endDraw(e, ctx_img, ratio);
+		}
+	});
+
+	cvs.addEventListener('touchstart', function(e){
+		clearTimeout(idleTime);
+		beginDraw(e, ctx, 1);
+		beginDraw(e, ctx_img, ratio);
+	});
+
+	cvs.addEventListener('touchmove', function(e){
+		endDraw(e, ctx, 1);
+		endDraw(e, ctx_img, ratio);
+	});
+	cvs.addEventListener('touchend', function(e){
+		idleTime = setTimeout(process, 1000)
+	});
+// *************************************************************
+
+	var assert = function (left, right) {
+		if (left === right) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	var a = getDelta([0.5, 0.5], 1, [1, 2]);
+	console.log(assert(a[0], 0.5));
+	console.log(assert(a[1], -0.5));
+
+	
+	var createMatrix = function (m, n, num) {
+		var matrix = [];
+		var i, j;
+		for (i = 0; i < m; i++) {
+			matrix[i] = [];
+			for (j = 0; j < n; j++) {
+				matrix[i][j] = num;
+			}
+		}
+		return matrix;
+	};
+	
+	var multiply = function (A, B) {
+		var matrix = createMatrix(A.length, B[0].length, 0);
+		var i, j, k;
+
+		if (A[0].length !== B.length) {
+			console.log("error: matrix dimensions do not agree");
+			return undefined;
+		}
+		for (i = 0; i < A.length; i++) {
+			for (j = 0; j < B[0].length; j++) {
+				for (k = 0; k < A[0].length; k++) {
+					matrix[i][j] += A[i][k] * B[k][j];
+				}
+			}
+		}
+		return matrix;
+	};
+
+	var product = function (v, w) {
+		if (v.length !== w.length) {
+			console.log("error: vector dimensions do not agree");
+			return undefined;
+		}
+		var sum = 0;
+		for (var i = 0; i < v.length; i++) {
+			sum += v[i] * w[i];
+		};
+		return sum;
+	};
+
+	function equal(A, B) {
+		for (var i = 0; i < A.length; i++) {
+			for (var j = 0; j < A[i].length; j++) {
+				if (A[i][j] !== B[i][j]) {
+					return false;
+				};
+			};
+		};
+		return true;
+	};
+
+});
---
+++ public/js/wuzi.js
@@ -0,0 +1,489 @@
+angular.module("wuziApp", []).controller("boardController" , function ($scope) {
+	function Action (from, to) {
+			this.to = to;
+			this.from = from;
+			this.utility = 0;
+			this.reverse = function () {
+				return {from: this.to, to: this.from};
+			};
+	};
+	var createBoard = function (m, n) {
+		var width = n;
+		var height = m;
+		var size = n * m;
+		var whites = [];
+		var blacks = [];
+		var map = [];
+		var i;
+		var path = [];
+		var utility = 0;
+		var stack = new Array();
+		var nextAction;
+		
+
+
+		function init() {
+			var i;
+			for (i = 0; i < size; i++) {
+				map[i] = 0;
+			};
+
+			for (i = 0; i < width; i++) {
+				whites[i] = i;
+				map[i] = 1;
+				blacks[i] = size + i - width;
+				map[size + i - width] = -1;
+			};
+		};
+
+		function getIndex (x, y) {
+			return x + y * width;
+		};
+
+		function getPieces (color) {
+			var ret = [];
+			for (var i = 0; i < map.length; i++) {
+				if (map[i] === color) {
+					ret.push(i);
+				};
+			};
+			return ret;
+		}
+		function findColor (x, y) {
+			var i;
+			var idx = getIndex(x, y);
+			for (i = 0; i < width; i++) {
+				if (whites[i] === idx) {
+					return 1;
+				} else if (blacks[i] === idx) {
+					return -1;
+				}
+			}
+			return 0;
+		};
+
+		/*function move (x, y, dx, dy) {
+			var color = findColor(x, y);
+			console.log(findColor(x, y), getIndex(x, y), getIndex(dx, dy));
+			map[getIndex(x, y)] = 0;
+			map[getIndex(dx, dy)] = color;
+			if (color === 1) {
+				whites[whites.indexOf(getIndex(x, y))] = getIndex(dx, dy);
+			} else if (color === -1) {
+				blacks[blacks.indexOf(getIndex(x, y))] = getIndex(dx, dy);
+			}
+			return color;
+		};*/
+
+		function move (action, map) {
+			if (map[action.to] !== 0) {
+				console.log("error: destination is occupied")
+				return false;
+			}
+			map[action.to] = map[action.from];
+			map[action.from] = 0;
+			/*var pieces = (map[action.to] === 1) ? whites : blacks;
+			var i = pieces.indexOf(action.from);
+			if (i !== -1) {
+				pieces[i] = action.to;
+			}*/
+		};
+
+		function moveByIdx (origin, dest, map) {
+			var temp = map[origin];
+			map[dest] = temp;
+			map[origin] = 0;
+			return temp;
+		};
+
+		function getTarget (idx, map) {
+			var ret = [];
+			if (idx - width >= 0 && map[idx - width] == 0) {
+				ret.push(idx - width);
+			}
+			if (idx % width != width - 1 && map[idx + 1] == 0) {
+				ret.push(idx + 1);
+			}
+			if (idx + width < size && map[idx + width] == 0) {
+				ret.push(idx + width);
+			}
+			if (idx % width != 0 && map[idx - 1] == 0) {
+				ret.push(idx - 1);
+			}
+			return ret;
+		};
+
+		//get color of location x, y
+		function get (array, x, y) {
+			if (x >= width || x < 0 || y >= width || y < 0) {
+				return 0;
+			}
+			return array[x + y * width];
+		};
+
+		function idxToAxis (idx) {
+			return {x: idx % width, y: Math.floor(idx / width)};
+		};
+
+		function copyMap (map) {
+			var ret = [];
+			for (var i = 0; i < map.length; i++) {
+				ret[i] = map[i]
+			};
+			return ret;
+		};
+
+		function eat (targetArray, map) {
+			/*var pieces;
+			if (map[targetArray[0]] === 1) {
+				pieces = whites;
+			} else {
+				pieces = blacks;
+			};*/
+			for (var i = 0; i < targetArray.length; i++) {
+				//pieces.splice(pieces.indexOf(targetArray[i]), 1);
+				map[targetArray[i]] = 0;
+			};
+		};
+
+		function eatTest (action, tempMap) {
+			var idx = action.to;
+			var orginIdx = action.from;
+			var ret = [];
+			var axis, x, y, temp;
+			if (tempMap[idx] != 0) {
+				return ret;
+			}
+			temp = tempMap[idx];
+			tempMap[idx] = tempMap[orginIdx];
+			tempMap[orginIdx] = 0;
+			
+			axis = idxToAxis(idx);
+			x = axis.x;
+			y = axis.y;
+			//console.log("tempMap", tempMap, axis);
+			if (tempMap[idx] === get(tempMap, x, y - 1)) {
+				if (tempMap[idx] + get(tempMap, x, y - 2) === 0 && get(tempMap, x, y + 1) === 0 && get(tempMap, x, y - 3) === 0) {
+					ret.push(getIndex(x, y - 2));
+				}
+				if (tempMap[idx] + get(tempMap, x, y + 1) === 0 && get(tempMap, x, y + 2) === 0 && get(tempMap, x, y - 2) === 0) {
+					ret.push(getIndex(x, y + 1));
+				}
+			}
+			if (tempMap[idx] === get(tempMap, x, y + 1)) {
+				if (tempMap[idx] + get(tempMap, x, y - 1) === 0 && get(tempMap, x, y + 2) === 0 && get(tempMap, x, y - 2) === 0) {
+					ret.push(getIndex(x, y - 1));
+				}
+				if (tempMap[idx] + get(tempMap, x, y + 2) === 0 && get(tempMap, x, y + 3) === 0 && get(tempMap, x, y - 1) === 0) {
+					ret.push(getIndex(x, y + 2));
+				}
+			}
+			if (tempMap[idx] === get(tempMap, x + 1, y)) {
+				if (tempMap[idx] + get(tempMap, x + 2, y) === 0 && get(tempMap, x - 1, y) === 0 && get(tempMap, x + 3, y) === 0) {
+					ret.push(getIndex(x + 2, y));
+				}
+				if (tempMap[idx] + get(tempMap, x - 1, y) === 0 && get(tempMap, x - 2, y) === 0 && get(tempMap, x + 2, y) === 0) {
+					ret.push(getIndex(x - 1, y));
+				}
+			}
+			if (tempMap[idx] === get(tempMap, x - 1, y)) {
+				if (tempMap[idx] + get(tempMap, x + 1, y) === 0 && get(tempMap, x - 2, y) === 0 && get(tempMap, x + 2, y) === 0) {
+					ret.push(getIndex(x + 1, y));
+				}
+				if (tempMap[idx] + get(tempMap, x - 2, y) === 0 && get(tempMap, x - 3, y) === 0 && get(tempMap, x + 1, y) === 0) {
+					ret.push(getIndex(x - 2, y));
+				}
+			}
+			tempMap[orginIdx] = tempMap[idx];
+			tempMap[idx] = temp;
+			return ret;
+			
+		};
+
+		/*
+		** get the utility after adding given utility
+		*/
+		function getPathUtility (given) {
+			return utility + given;
+			
+		}
+
+		/*
+		** get the number of pieces that given node can eat 
+		*/
+		function getActionUtility (action, map) {
+			return eatTest(action, map).length;
+		}
+
+		/*
+		*
+		*/
+
+		function getBestChildren (pieces, origin, map) {
+			var i, temp;
+			var best = -1;
+			var utl = 0;
+			for (i = 0; i < pieces.length; i++) {
+				temp = getActionUtility(pieces[i], origin, map);
+				if (utl < temp) {
+					best = i;
+					utl = temp;
+				};
+			}
+			return {index: best, utility: utl};
+		};
+
+
+		function getActions (player, map) {
+			var a = [];
+			var dest = [];
+			var i, j;
+			var pieces = (player === 1) ? getPieces(1) : getPieces(-1);
+			for (i = 0; i < pieces.length; i++) {
+				dest = getTarget(pieces[i], map);
+				for (j = 0; j < dest.length; j++) {
+					a.push(new Action(pieces[i], dest[j]));
+				};
+			};
+			return a;
+		};
+
+		function movePiece (player, action) {
+			var pieces = (map[action.from] === 1) ? getPieces(1) : getPieces(-1);
+			pieces[pieces.indexOf(action.from)] = action.to;
+		};
+		
+		function copy (array) {
+			var ret = [];
+			for (var i = 0; i < array.length; i++) {
+				ret[i] = array[i]
+			};
+			return ret;
+		};
+
+		function DFS (player, map, depth) {
+			var actions = getActions(player, map);
+			if (actions.length === 0) {
+				return 0;
+			}
+			var i, j, ate, tempTarget, temp, e;
+			var index = 0;
+			var result = -10000, u, ret = [];
+			var resultArray = [];
+			if (depth === 0) {
+				result = 0;
+				for (i = 0; i < actions.length; i++) {
+					actions[i].utility = getActionUtility(actions[i], map);
+					//resultArray.push(actions[i].utility);
+					//if (result < actions[i].utility) {
+					result += actions[i].utility;
+						//index = i;
+					//};
+					/*stack.push(actions[i].from + " to " + actions[i].to);
+					stack.push(actions[i].utility);
+					console.log("stack", stack);
+					stack.pop();
+					stack.pop();*/
+				};
+				//console.log("stack", stack);
+			} else {
+				for (i = 0; i < actions.length; i++) {
+					e = 1;
+					stack.push(actions[i].from + " to " + actions[i].to);
+					ate = eatTest(actions[i], map);
+					move(actions[i], map);
+					temp = map[actions[i].to];
+					if (ate.length !== 0){
+						eat(ate, map);
+						e += 10 * ate.length;
+					}
+
+					ret.push(DFS(- player, map, depth - 1) * -1 + e);
+					if (result < ret[ret.length - 1]) {
+						result = ret[ret.length - 1];
+						index = i;
+					}
+					if (ate.length !== 0){
+						for (j = 0; j < ate.length; j++) {
+							map[ate[j]] = - temp;
+							//result += 10 * temp;
+						}
+					}
+					stack.pop();
+					move(actions[i].reverse(), map);	
+				}
+				//console.log("ret", ret, player, actions[ret.indexOf(result)], result, depth);
+			}
+			nextAction = actions[ret.indexOf(result)];
+			return result;
+		};
+
+		init();
+		return {
+			w: function (x, y) {
+				return whites;
+			},
+			b: function (x, y) {
+				return blacks;
+			},
+			moveUp: function (x, y) {
+				move(x, y, x, y - 1);
+			},
+			moveDown: function (x, y) {
+				return move(x, y, x, y + 1);
+			},
+			getMap: function () {
+				return map;
+			},
+			getWhites: function () {
+				return getPieces(1);
+			},
+			getBlacks: function () {
+				return getPieces(-1);
+			},
+			findColor: function (x, y) {
+				return findColor(x, y);
+			},
+			moveByIdx: function (idx1, idx2, map) {
+				return moveByIdx(idx1, idx2, map);
+			},
+			getTarget: function (idx, map) {
+				return getTarget(idx, map);
+			},
+			eatTest: function (action, map) {
+				var temp = copyMap(map);
+				return eatTest(action, temp);
+			},
+			DFS: function (p, m, d) {
+				return DFS(p, m, d);
+			},
+			stack: function () {
+				return stack;
+			},
+			move: function (a, m) {
+				return move(a, m);
+			},
+			eat: function (array, map) {
+				return eat(array, map);
+			},
+			getNextAction: function (player, map, depth) {
+				DFS(player, map, depth);
+				return nextAction;
+			}
+		};
+	};
+
+	var WIDTH = 5;
+	var board = createBoard(WIDTH, WIDTH);
+	var unitWidth = 900 / WIDTH;
+	var map = board.getMap();
+	var selectedWhite, selectedBlack, selected;
+	var w = board.getWhites();
+	var b = board.getBlacks();
+	var t = [];
+	var horizaontalLines = [];
+	var verticalLines = [];
+	var SIZE = 25;
+	var i;
+	$scope.targets = [];
+	$scope.mode = 0;
+	for (i = 0; i < WIDTH; i++) {
+		horizaontalLines[i] = {x1:unitWidth / 2, y1:unitWidth / 2 + i * unitWidth, x2:900 - unitWidth / 2, y2:unitWidth / 2 + i * unitWidth};
+		verticalLines[i] = [unitWidth / 2 + i * unitWidth, unitWidth / 2, unitWidth / 2 + i * unitWidth, 900 - unitWidth / 2];
+	}
+	var trans = function (array, width) {
+		var ret = [];
+		for (var i = 0; i < array.length; i++) {
+			ret[i] = {};
+			ret[i].x = array[i] % WIDTH * unitWidth + unitWidth / 2;
+			ret[i].y = Math.floor(array[i] / WIDTH) * unitWidth + unitWidth / 2;
+			ret[i].selected = false;
+		};
+		return ret;
+	};
+	var setAllFalse = function (array) {
+
+	};
+
+
+	$scope.selectWhite = function (idx) {
+		if ($scope.mode === 0) {
+			return 0;
+		}
+		if (selectedWhite !== undefined) {
+			$scope.whites[selectedWhite].selected = false;
+		}
+		$scope.whites[idx].selected = true;
+		w = board.getWhites();
+		selectedWhite = idx;
+		selected = w[idx];
+		console.log("************************************************************************");
+		console.log("map", map, board.w())
+		t = board.getTarget(selected, map);
+		//console.log(t);
+		$scope.targets = trans(t, WIDTH);
+		//board.DFS(1, map, 4);
+		console.log(board.getNextAction(1, map, 4));
+		//console.log(tree);
+		//console.log(us);
+	};
+	$scope.selectBlack = function (idx) {
+		if (selectedBlack !== undefined) {
+			$scope.blacks[selectedBlack].selected = false;
+		}
+		b = board.getBlacks();
+		$scope.blacks[idx].selected = true;
+		selectedBlack = idx;
+		selected = b[idx];
+		console.log("********************************************************************************");
+		console.log("map", map, board.b());
+		t = board.getTarget(selected, map);
+		$scope.targets = trans(t, WIDTH);
+		//board.DFS(-1, map, 4);
+		console.log(board.getNextAction(-1, map, 4));
+		//console.log(tree);
+		//console.log(us);
+	};
+
+
+	$scope.moveToTarget = function (idx) {
+		//console.log($scope.targets[idx]);
+		
+		var action = new Action(selected, t[idx]);
+		var ate = board.eatTest(action, map);
+		var a;
+		board.eat(ate, map);
+		board.move(action, map);
+		
+		
+		$scope.blacks = trans(board.getBlacks(), WIDTH);
+		$scope.whites = trans(board.getWhites(), WIDTH);
+		$scope.targets = [];
+		//console.log("selected", selected);
+		a = board.getNextAction(1, map, 4);
+		
+		ate = board.eatTest(a, map);
+		board.eat(ate, map);
+		board.move(a, map);
+		$scope.blacks = trans(board.getBlacks(), WIDTH);
+		$scope.whites = trans(board.getWhites(), WIDTH);
+		//console.log(w, b);
+		//console.log(board.getMap());
+		//console.log("t", t, "idx", idx);
+		//board.stack().push({from: selected, to:t[idx]});
+		//console.log("stack", board.stack(), "t[0]", t[idx]);
+		
+	};
+	
+	$scope.whites = trans(w, WIDTH);
+	$scope.blacks = trans(b, WIDTH);
+
+	var p = 0;
+	$(".piece").click(function () {
+		p += 200;
+		$(".piece").animate({left: p + "px"});
+	});
+});
+
+
+

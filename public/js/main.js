095f3bbda6e8cf5cbc05bf34deb9a743403107db
Yang2008 <ywang248@gmail.com>
2015-08-31 23:51:03 -0400

    adjust index.html UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,24 @@
 var strings = {};
 strings.cn = [
-	"幼教考试试题", 
-	"五子棋", 
-	"手写识别", 
-	"代购网站", 
-	"代购网站.新",
-	"代购网站（手机版）",
-	"计时器",
-	"个人网站（旧）",
+	"幼教考试试题 (更新: 2015-07-25)", 
+	"五子棋 (更新: 2015-03-20)", 
+	"手写识别 (更新: 2015-03-21)", 
+	"代购网站 (更新: 2015-06-15)", 
+	"代购网站.新 (更新: 2015-08-30)",
+	"代购网站（手机版） (更新: 2015-07-29)",
+	"计时器 (更新: 2015-07-25)",
+	"个人网站（旧） (更新: 2015-03-29)",
 	"邮箱"
 ];
 strings.en = [
-	"Preschool Teacher Test Questions", 
-	"Wu Zi Qi", 
-	"Chinese Hand Writing Recognition", 
-	"Online Store", 
-	"New Online Store",
-	"New Online Store(mobile version)",
-	"Timer",
-	"Personal WebSite",
+	"Preschool Teacher Test Questions (update: 2015-07-25)", 
+	"Wu Zi Qi (update: 2015-03-20)", 
+	"Chinese Hand Writing Recognition (update: 2015-03-21)", 
+	"Online Store (update: 2015-06-15)", 
+	"New Online Store (update: 2015-08-30)",
+	"New Online Store(mobile version) (update: 2015-07-29)",
+	"Timer (update: 2015-07-25)",
+	"Personal WebSite (update: 2015-03-29)",
 	"Email"
 ];
 

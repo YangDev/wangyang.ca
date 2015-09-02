743d2c24ab3f656fea69c8f12677885da7c8ca51
Yang2008 <ywang248@gmail.com>
2015-09-02 17:01:08 -0400

    add compression, change logger

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+"use strict;" 
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var compression = require('compression')
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,9 +17,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(compression());
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
+var connectionString = "mongodb://root:root@ds041603.mongolab.com:41603/users";
+//var connectionString = "mongodb://MongoLab-6:mbYjxpVI4dL7L5VKSfOld2T8HVRChMDc8ESqbISz1GE-@ds052827.mongolab.com:52827/MongoLab-6";
+mongoose.connect(connectionString);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
